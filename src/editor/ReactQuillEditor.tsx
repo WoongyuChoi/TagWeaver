810b72b4878c212aff9d9a648eff47609ec945c2
WoongyuChoi <schna@kakao.com>
2025-10-22 14:42:33 +0900

    feature: (Quill.js)quill-table-better 테이블 기능 추가

--- src/editor/ReactQuillEditor.tsx
+++ src/editor/ReactQuillEditor.tsx
@@ -1,7 +1,15 @@
-import { useEffect, useMemo, useRef } from "react";
 import { Box } from "@mui/material";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import QuillTableBetter from "quill-table-better";
+import "quill-table-better/dist/quill-table-better.css";
+import { useEffect, useMemo, useRef } from "react";
+// import ReactQuill, { Quill } from "react-quill";
+import ReactQuill, { Quill } from "react-quill-new";
+// import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
+
+if ((Quill as any)?.register) {
+  (Quill as any).register({ "modules/table-better": QuillTableBetter }, true); // overwrite 허용
+}
 
 type Props = {
   initialHtml?: string;
@@ -10,28 +18,64 @@ type Props = {
 
 const ReactQuillEditor = ({ initialHtml = "<p></p>", onChange }: Props) => {
   const ref = useRef<ReactQuill | null>(null);
-  
-  const modules = useMemo(() => ({
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ align: [] }],
-      ["link", "code-block", "clean"],
-      [{ color: [] }, { background: [] }],
-      ["table"]
-    ],
-  }), []);
+
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ["bold", "italic", "underline"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          // { indent: "-1" },
+          // { indent: "+1" },
+        ],
+        [{ align: [] }],
+        ["link", "code-block", "clean"],
+        [{ color: [] }, { background: [] }],
+        // [{ script: 'sub' }, { script: 'super' }],
+        ["table-better"],
+      ],
+      clipboard: { matchVisual: false },
+      table: false,
+      "table-better": {
+        language: "en_US",
+        menus: ["column", "row", "merge", "table", "cell", "wrap", "copy", "delete"],
+        toolbarTable: true,
+      },
+      keyboard: {
+        bindings: (QuillTableBetter as any).keyboardBindings,
+      },
+    }),
+    []
+  );
 
   const formats = [
-    "header","bold","italic","underline",
-    "list","bullet","align","link","code-block","color","background"
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    // "strike",
+    // "blockquote",
+    "list",
+    // "indent",
+    "align",
+    "link",
+    // "script",
+    "code-block",
+    "color",
+    "background",
+    // "table","table-better","table-row","table-cell","table-col"
   ];
 
+  const didInitRef = useRef(false);
   useEffect(() => {
+    if (didInitRef.current) return;
+    didInitRef.current = true;
     const inst = ref.current?.getEditor();
     if (!inst) return;
-    const current = ref.current!.editor!.root.innerHTML;
+    // const current = ref.current!.editor!.root.innerHTML;
+    const current = (ref.current!.editor!.root as HTMLElement).innerHTML;
     if (current !== initialHtml) {
       inst.clipboard.dangerouslyPasteHTML(initialHtml);
     }
@@ -46,7 +90,7 @@ const ReactQuillEditor = ({ initialHtml = "<p></p>", onChange }: Props) => {
         // defaultValue={initialHtml}
         onChange={(html) => onChange?.(html)}
         modules={modules}
-        formats={formats}
+        // formats={formats} // 모든 포맷 허용
         style={{ height: "100%" }}
       />
     </Box>
